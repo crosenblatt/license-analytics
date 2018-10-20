04e82b79b6b09410c53b761dd2567721920b577e
crosenblatt <rosench13@gmail.com>
2018-10-20 01:49:36 -0400

    parse data into json

--- license-analytics/src/App.js
+++ license-analytics/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Stitch } from 'mongodb-stitch-browser-sdk';
+
+const client = Stitch.initializeDefaultAppClient('license-analytics-jpmyw');
 
 class App extends Component {
   constructor(props) {
     super(props)
-    const regex = /%(\w{2})([^\^]*)\^([^\$]*)\$([^\$]*)\$([^\$]*)\$\^([^\^]*)\^\?\;(\d{6})(\d*)\=(\d{2})(\d{2})(\d{4})(\d{2})(\d{2})\?\+10(\d{9})  (\w) (\w)             (\d)(\d)(\d{2})(\d{3})(\w{3})(\w{3})/
   
     this.state = {
       field_data: ""
@@ -17,8 +18,34 @@ class App extends Component {
 
   handleSubmit(event) {
     this.data = this.state.field_data;
-    console.log(this.data);
     this.setState({field_data: ""})
+    let res = this.data.match(/%(\w{2})([^\^]*)\^([^\$]*)\$([^\$]*)\$([^\$]*)\$\^([^\^]*)\^\?\;(\d{6})(\d*)\=(\d{2})(\d{2})(\d{4})(\d{2})(\d{2})\?\+10(\d{9})  (\w) (\w)             (\d)(\d)(\d{2})(\d{3})(\w{3})(\w{3})/)
+    
+    let split_data = {
+      "state": res[1],
+      "city": res[2],
+      "last_name": res[3],
+      "first_name": res[4],
+      "middle_name": res[5],
+      "address": res[6],
+      "iin": res[7],
+      "license_number": res[8],
+      "expiration_year": res[9],
+      "expiration_month": res[10],
+      "dob_year": res[11],
+      "dob_month": res[12],
+      "dob_day": res[13],
+      "zip_code": res[14],
+      "license_class": res[15],
+      "license_restriction": res[16],
+      "gender": res[17],
+      "height_ft": res[18],
+      "height_in": res[19],
+      "weight": res[20],
+      "hair_color": res[21],
+      "eye_color": res[22]
+    }
+
     event.preventDefault();
   }
 
